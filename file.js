d5b2e14dd17b091425f0f322abe72d80f7a24ba6
Kevin Luch <79926777+KevinLuch@users.noreply.github.com>
2021-10-02 13:56:01 -0500

    first commit

---
+++ file.js
@@ -0,0 +1,28 @@
+// Bubble sort 
+
+// Create the bblSort function
+function bblSort(arr) {
+    for(var i = 0; i < arr.length; i++) {
+        // Last i elements are already in place
+        for (var j = 0; j < ( arr.length - i -1); j++) {
+
+            // Checking if the item at present iteration
+            // is greater than the next iteration
+            if(arr[j] > arr[j+1]) {
+                // if the condition is true then swap them
+                var temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+            }
+        }
+    }
+    // Print the sorted array
+    console.log(arr);
+}
+// This is our unsroted array
+var arr = [234, 43, 55, 63, 5, 6, 235, 547];
+var arr = [69, 89, 22, 1, 1000, 99, 235, 775];
+
+// Now pass this array to the bblSort() function
+bblSort(arr)
+
